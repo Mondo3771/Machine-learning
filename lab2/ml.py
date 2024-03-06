8a5455b10eea3ce0ccdc33e0bb8fff4f789db183
Mondo3771 <kabelomrn@gmail.com>
2024-03-06 21:46:54 +0200

    did the probabuty

--- lab2/ml.py
+++ lab2/ml.py
@@ -1,8 +1,20 @@
 import numpy as np
-# def laplace(s, count, county):
-#     return (count + 1)/(county + 2)
-
-# def no_laplace():
+def prob(v , d, b, cp):
+    p=1
+    # print(d)
+    # print(v)
+    for i in range(0,len(v)):
+        if i == 1 and b[i] in d:
+            p = p*d[b[i]]
+            # print(d[b[i]])
+        elif b[i]  in d:
+            p=p*(1-d[b[i]])
+    #         print(d[b[i]])
+    #     print(p)
+    #     print(b[i])
+    #     print("]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]")
+    # print("done")
+    return p
 
 
 file1 = open("simple-food-reviews.txt")
@@ -12,12 +24,6 @@ while len(words) != 0:
     reviews = np.append(reviews,words)
     words = file1.readline()[:-1]
 np.random.shuffle(reviews)
-#print(reviews)
-# bag= np.array()
-# for i in range(0,len( reviews)-1):
-#     rev = reviews[i].split(" ")
-
-# print (rev)
 countp=0
 countn=0
 
@@ -25,46 +31,73 @@ bags=np.array([])
 dictP={}
 dictN={}
 
-
 for i in range(12):
-
-    a=reviews[i].split()[1:]
-
+    a=np.array(reviews[i].split()[1:])
+    a= np.unique(a)
     bags=np.append(bags,a)
     
     if reviews[i][0]=='1':
         countp+=1
         for j in a:
-            # print(dictP[j])
             if j not in dictP:
                 dictP.update({j:1})
             else:
                 dictP.update({j:dictP[j]+1})    
     else:
-      
         for k in a:
-            # print(k not in dictN)
             if k not in dictN:
                 dictN.update({k:1})
             else:
                 dictN.update({k:dictN[k]+1}) 
         countn+=1
-# print(dictP)
-# print(dictN)
-
+print(dictN)
+print(dictP)
 for k in dictP:
     dictP.update({k:dictP[k]/countp})
 
 for k in dictN:
     dictN.update({k:dictN[k]/countn})
-
-print(np.unique(bags))
-print(dictP)
-print(dictN)
-
+# print(bags)
+bags= np.unique(bags)
 priorP=countp/(countp+countn)
 priorN=countn/(countp+countn)
-
+for j in range(12,13):
+    words = reviews[j]
+    rev= words.split(" ")
+    # print(bags)
+    # print('h')
+    # print(rev)
+    vec = np.zeros(len(bags))
+    # print(rev[1])
+    # an = np.where(bags == rev[1])
+    # print(len(an[0]))
+    # print(an[0])
+    lap ={}
+    for i in range(1,len(rev)):
+        # print(rev[i])
+        an = np.where(bags == rev[i])[0]
+        if len(an) ==0:
+            print("hello")
+        else:
+            for k in an:
+                vec[k]+=1
+    # print(vec)
+    # print(dictP)
+    # print(bags)
+                
+    npr= prob(vec, dictN, bags,countn)
+    pp= prob(vec, dictP,bags,countp )
+    print(pp)
+    print(npr)
+    print(words)
+    print(npr)
+    print(']]]]]]]]]]]]]]]]]]]]]]]]]]]')
+    print(pp)
+    print(words)
+    print(priorP*pp)
+    print(priorN*npr)
+    p= (priorN*npr)/(priorN*npr + priorP*pp)
+    print(p)
 
 
 
