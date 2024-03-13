74234db653f69562b91b6808e6cb38d7c44f9862
Tapiwa <mazaruratapiwa03@gmail.com>
2024-03-13 13:27:43 +0200

    Second commit

--- lab2/Q4.py
+++ lab2/Q4.py
@@ -1,35 +1,30 @@
 import numpy as np 
+
 books = np.array([])
 bookDictionaries = []
 
 bookTotalWords = []
-# print("h")
-bagOfWords = np.array([])
+bagOfWords = []
 
-
-sum =0
-# print("h")
 for i in range(1,7):
     file = open("HP" + str(i) + ".txt")
-    # print("h")
+
     books = np.append(books,file.read())
     words = books[i-1].split(" ")
-    print("a")
 
     dictionary = {}
     numWords = 0
-    # print("h")
+
     for word in words:
         numWords += 1
-        # bagOfWords = np.append(bagOfWords,word)
+        bagOfWords.append(word)
         if word in dictionary:
             dictionary[word] += 1
         else:
             dictionary[word] = 1    
-    sum += numWords
-    # print("h")
     bookDictionaries.append(dictionary)
     bookTotalWords.append(numWords)
 
-print(sum)
-print(len(bagOfWords))
+bagOfWords = list(set(bagOfWords))    #List of all the unique words in the books to make vectors
+
+print(bagOfWords)
