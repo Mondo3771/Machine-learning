e6ae87cfc513039c03851e73ee6d92766c7d0c6f
Mondo3771 <kabelomrn@gmail.com>
2024-03-13 11:16:42 +0200

    chnged orders

--- lab2/ml.py
+++ lab2/ml.py
@@ -6,7 +6,7 @@ def add_dict(bag,review,dictN,dictP,countN,countP):
     reviewWords =np.array(review.split()[1:])
     reviewWords = np.unique(reviewWords) #gets rid of duplicates in the review bagecause we are checking if its in a neg rev not hoe many times its in the review
     bag=np.append(bag,reviewWords)
-    a = np.setdiff1d(a,nono)
+    reviewWords = np.setdiff1d(reviewWords,nono)
     if review[0]=='1':
         countP+=1
         for j in reviewWords:
