e9703f1592755b6d0f03f3ddfc16d6957f15654d
Mondo3771 <kabelomrn@gmail.com>
2024-03-16 22:50:12 +0200

    completed 3b , still need to do 3a

--- lab3/ml.py
+++ lab3/ml.py
@@ -7,7 +7,6 @@ data_input = pd.read_csv('banknote_authentication.csv')
 # Convert the data to a NumPy array
 X = data_input.to_numpy()
 
-# Print the shape of the data
 data = np.array(X[0][0].split(';'))
 for i in range(1, len(X)):
     data = np.vstack((data,np.array(X[i][0].split(';'))))
